fee8c0b747e6f8ecb21a99b20c4140bb8956b31f
유현정/(협력사) 차장/플랫폼관리솔루션개발Task(hyunjung.ryu@lgepartner.com) <hyunjung.ryu@lge.com>
2023-03-09 16:28:00 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import * as React from 'react';
-import config from './constants/aimbleConfig';
+import { Route, Routes, useNavigate } from "react-router-dom";
 import './styles/aimble.scss';
+import config from './constants/aimbleConfig';
 import logo from './assets/img/Aimble-Portfolio.png';
 import Portal from './pages/AAASample';
+import { Button } from '@mui/material';
 
 function App() {
-  return <Portal {...config} logo={logo} />;
+
+  let navigate = useNavigate();
+  
+  function handleClick() {
+    navigate("/home");
+  }
+
+  return (
+    <>
+      <Button onClick={handleClick}>Aimble</Button>
+      <Routes>
+        <Route path='/' element={<h1>Home Page Component</h1>} />
+        <Route path='login' element={<h1>Login Page Component</h1>} />     
+      </Routes>
+      {/* <Portal {...config} logo={logo} /> */}
+    </>        
+  )    
 }
 
 export default App;
